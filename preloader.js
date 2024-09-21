67d1e34dea6fac7e78e81a4d0f95f5e563e4d6dd
Ushtv <166837899+Ushtv@users.noreply.github.com>
2024-09-21 15:32:55 +0100

    Add files via upload
    Create Image Folder

---
+++ preloader.js
@@ -0,0 +1,6 @@
+// Wait for 2 seconds before hiding the preloader
+setTimeout(function() {
+    document.body.classList.remove('loading');
+  }, 4000);
+
+//   document.body.classList.add('loading');
